006de0688b662a21436c0609811677a97db3fdd0
tangledsnakes24 <cbradshaw@commschool.org>
2024-12-16 09:35:54 -0500

    Update day2.cpp

--- day2.cpp
+++ day2.cpp
@@ -5,9 +5,13 @@ int main()
 {
     int x; // create x variable, int indicates that the thing stored will be intiger
     x = 5; // store value 5  in x  
+    // the above steps can be combined by doing int x = 5;
+    int y = 6;
 
-    std::cout << x; // display value stored in x
-    
+
+    std::cout << x << "\n"; // display value stored in x
+    std::cout << y << "\n";
+
 
 
 
