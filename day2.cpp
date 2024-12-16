f3b8a27f919efb743e4b4c2a438b7c90bbebd29b
tangledsnakes24 <cbradshaw@commschool.org>
2024-12-16 09:33:31 -0500

    Create day2.cpp

---
+++ day2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+int main() 
+{
+    int x; // create x variable, int indicates that the thing stored will be intiger
+    x = 5; // store value 5  in x  
+
+    std::cout << x; // display value stored in x
+    
+
+
+
+
+    return 0;
+}
